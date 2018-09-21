20825e86491b902a81d9894b4eaf1ca49203bb1b
singhsauravsk <singhsauravsk@gmail.com>
2018-09-21 20:48:25 +0530

    Add files via upload

---
+++ java/com/zycus/application/Launcher.java
@@ -0,0 +1,14 @@
+package com.zycus.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Launcher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Launcher.class, args);
+	}
+}
